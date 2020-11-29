c2c7074d158c82d04cf8a4b22e0141e794e1f6ad
Baptiste <bat.boleat13@gmail.com>
2020-11-29 10:00:59 +0100

    add pseudos

--- js/roomServer.js
+++ js/roomServer.js
@@ -4,8 +4,8 @@ class RoomServer {
     roomId;
 
     constructor() {
-        //this.ws = new WebSocket('ws://localhost:3000');
-        this.ws = new WebSocket('wss://blindtest-chaf.herokuapp.com/');
+        this.ws = new WebSocket('ws://localhost:3000');
+        //this.ws = new WebSocket('wss://blindtest-chaf.herokuapp.com/');
         this.msgHandlers = {};
         this.keepAlive();
         this.ws.onopen = () => { console.log("Connected to room server"); };
