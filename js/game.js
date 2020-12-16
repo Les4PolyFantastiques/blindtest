cf6209eac7f5a80474eb444e58f02e82e57701e8
manon-chaix <mchaix26@gmail.com>
2020-12-16 19:08:19 +0100

    fin timer

--- js/game.js
+++ js/game.js
@@ -6,6 +6,42 @@ let reponsediv = document.getElementById("reponse-div");
 let reponseField = document.getElementById("reponseField");
 let texteReponse = document.getElementById("texteReponse");
 var tableauReponse = document.getElementById("tableauReponse");
+var dureeTimer = document.getElementById("timerField");
+var labelTimer = document.getElementById("texteTimer");
+
+// Start with an initial value of 20 seconds
+var TIME_LIMIT = 0;
+const FULL_DASH_ARRAY = 283;
+
+// Warning occurs at 10s
+const WARNING_THRESHOLD = 10;
+// Alert occurs at 5s
+const ALERT_THRESHOLD = 5;
+
+const COLOR_CODES = {
+  info: {
+    color: "green"
+  },
+  debut: {
+    color: "grey"
+  },
+  warning: {
+    color: "orange",
+    threshold: WARNING_THRESHOLD
+  },
+  alert: {
+    color: "red",
+    threshold: ALERT_THRESHOLD
+  }
+};
+
+// Initially, no time has passed, but this will count up
+// and subtract from the TIME_LIMIT
+let timeLeft = TIME_LIMIT;  
+let timerInterval = null;
+let timePassed = 0;
+
+let remainingPathColor = COLOR_CODES.debut.color;
 
 var player1;
 done = true;
@@ -22,6 +58,8 @@ function startGame(isCreator) {
         textDiv.innerText = "Waiting for the creator to select a playlist";
     } else {
         textDiv.innerText = "Please select a playlist";
+        dureeTimer.style.display = "block";
+        labelTimer.style.display = "block";
     }
     AmICreator = isCreator;
 
@@ -95,8 +133,12 @@ function onPlayerReady1(event) {
 
 function onPlayerStateChange1(event) {
     if (event.data == 1 && !done) {
-        setTimeout(stopVideo, 10000);
+        setTimeout(stopVideo, TIME_LIMIT*1000);
         done = true;
+        timeLeft = TIME_LIMIT;
+        timerInterval = null;
+        timePassed = 0;
+        startTimer();
     }
 }
 
@@ -130,12 +172,59 @@ function playNextMusic(data) {
     var ctrlq1 = document.getElementById("youtube-audio1");
     ctrlq1.dataset.video = data.token;
     player1.loadVideoById(ctrlq1.dataset.video);
+
+    document.getElementById("app").innerHTML = `
+    <div class="base-timer">
+        <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+            <g class="base-timer__circle">
+                <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45" />
+                <path
+                    id="base-timer-path-remaining"
+                    stroke-dasharray="0"
+                    class="base-timer__path-remaining ${remainingPathColor}"
+                    d="
+                    M 50, 50
+                    m -45, 0
+                    a 45,45 0 1,0 90,0
+                    a 30,30 0 1,0 -90,0
+                    "
+                ></path>
+            </g>
+        </svg>
+        <span id="base-timer-label" class="base-timer__label">
+            ${formatTimeLeft(timeLeft)}
+        </span>
+    </div>
+`;
 }
+
+function onTimesUp() {
+    clearInterval(timerInterval);
+  }
+  
+  function startTimer() {
+    timerInterval = setInterval(() => {
+      timePassed = timePassed += 1;
+      timeLeft = TIME_LIMIT - timePassed;
+      document.getElementById("base-timer-label").innerHTML = formatTimeLeft(
+        timePassed
+      );
+      setCircleDasharray();
+      setRemainingPathColor(timeLeft);
+  
+      if (timeLeft === 0) {
+        onTimesUp();
+      }
+    }, 1000);
+  }
 
 function submitPlaylist() {
     let playlistId = document.getElementById("submit-playlist-id").value;
     roomServer.emit("playlist", { roomId: roomServer.roomId, playlistId: playlistId });
     document.getElementById("submit-playlist").style.display = "none";
+    dureeTimer.style.display = "none";
+    labelTimer.style.display = "none";
+    TIME_LIMIT = Number(dureeTimer.value);
 }
 
 function revealAnswer(data) {
@@ -199,5 +288,60 @@ function envoyerReponse(){
     });
     reponsediv.style.display = "none";
 }
+
+function formatTimeLeft(time) { 
+    // Seconds are the remainder of the time divided by 60 (modulus operator)
+    let seconds = time % 60;
+    // If the value of seconds is less than 10, then display seconds with a leading zero
+    if (seconds < 10) {
+      seconds = `0${seconds}`;
+    }
+    // The output in SS format
+    return `${seconds}`;
+  }
+
+  // Divides time left by the defined time limit.
+  function calculateTimeFraction() {
+    const rawTimeFraction = timeLeft / TIME_LIMIT;
+    return 1 - (rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction));
+  }
+      
+  // Update the dasharray value as time passes, starting with 283
+  function setCircleDasharray() {
+    const circleDasharray = `${(
+      calculateTimeFraction() * FULL_DASH_ARRAY
+    ).toFixed(0)} 283`;
+    document
+      .getElementById("base-timer-path-remaining")
+      .setAttribute("stroke-dasharray", circleDasharray);
+  }
+
+  function setRemainingPathColor(timeLeft) {
+    const { alert, warning, info, debut } = COLOR_CODES;
+    document
+        .getElementById("base-timer-path-remaining")
+        .classList.remove(debut.color);
+    document
+        .getElementById("base-timer-path-remaining")
+        .classList.add(info.color);
+    // If the remaining time is less than or equal to 5, remove the "warning" class and apply the "alert" class.
+    if (timeLeft <= alert.threshold) {
+      document
+        .getElementById("base-timer-path-remaining")
+        .classList.remove(warning.color);
+      document
+        .getElementById("base-timer-path-remaining")
+        .classList.add(alert.color);
+  
+    // If the remaining time is less than or equal to 10, remove the base color and apply the "warning" class.
+    } else if (timeLeft <= warning.threshold) {
+      document
+        .getElementById("base-timer-path-remaining")
+        .classList.remove(info.color);
+      document
+        .getElementById("base-timer-path-remaining")
+        .classList.add(warning.color);
+    }
+  }
 
 boutonReponse.addEventListener("click", envoyerReponse);
